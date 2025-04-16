d43f1a36908dfb0285a5b5a7d34544b12a78a387
Smit-Mahesh-Panchal <spanch19@asu.edu>
2025-04-16 14:47:18 -0700

    Update GroceryApi2Application.java removed the unused import to pass all the jobs

--- src/main/java/edu/asu/assign4/rest/grocery/GroceryApi2Application.java
+++ src/main/java/edu/asu/assign4/rest/grocery/GroceryApi2Application.java
@@ -3,9 +3,6 @@ package edu.asu.assign4.rest.grocery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import to fail the checkstyle
-import edu.asu.assign4.rest.grocery.model.GroceryItem;
-
 @SpringBootApplication
 public class GroceryApi2Application {
 
